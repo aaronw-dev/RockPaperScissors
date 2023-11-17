95d04f08caff3591406ddce2718327d2a83857e2
AW_Dev <riptidegamesceo@gmail.com>
2023-11-16 17:27:51 -0800

    removed all underscores from code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const validchoices = ["1", "2", "3"];
+const validChoices = ["1", "2", "3"];
 const alignments = ["rock", "paper", "scissors"];
 
 const battles = {
@@ -17,27 +17,27 @@ const battles = {
 };
 
 function playRPS(choice) {
-  const bot_choice = Math.floor(Math.random() * 3);
+  const botChoice = Math.floor(Math.random() * 3);
 
-  const playerbattle = battles[alignments[parseInt(choice) - 1]];
-  const botbattle = battles[alignments[bot_choice]];
+  const playerBattle = battles[alignments[parseInt(choice) - 1]];
+  const botBattle = battles[alignments[botChoice]];
 
-  const playeraction = playerbattle.action;
-  const botaction = botbattle.action;
+  const playerAction = playerBattle.action;
+  const botAction = botBattle.action;
 
-  const playerbeats = playerbattle.beats;
-  const botbeats = botbattle.beats;
+  const playerBeats = playerBattle.beats;
+  const botBeats = botBattle.beats;
 
-  const bot_word = alignments[bot_choice];
-  const player_word = alignments[parseInt(choice) - 1];
+  const botWord = alignments[botChoice];
+  const playerWord = alignments[parseInt(choice) - 1];
 
-  if (playerbeats === bot_word) {
-    console.log(`${player_word.charAt(0).toUpperCase() + player_word.slice(1)} ${playeraction} ${bot_word}`);
+  if (playerBeats === botWord) {
+    console.log(`${playerWord.charAt(0).toUpperCase() + playerWord.slice(1)} ${playerAction} ${botWord}`);
     console.log("Player wins");
-  } else if (botbeats === player_word) {
-    console.log(`${bot_word.charAt(0).toUpperCase() + bot_word.slice(1)} ${botaction} ${player_word}`);
+  } else if (botBeats === playerWord) {
+    console.log(`${botWord.charAt(0).toUpperCase() + botWord.slice(1)} ${botAction} ${playerWord}`);
     console.log("Bot wins");
-  } else if (bot_word === player_word) {
+  } else if (botWord === playerWord) {
     console.log("Draw!");
   }
 }
